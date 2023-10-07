f35927f457e709fcc6163effed428c95ae2a36a0
BRIAN MAHER <bmaher22@mac.comclear>
2023-10-06 20:45:22 -0400

    anotha one

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
- var APIkey = '1f5fe3c07897e528af5a10f8fb5ea2e7';
+var APIkey = '1f5fe3c07897e528af5a10f8fb5ea2e7';
 
 // when I'm using this app, and search for the weather conditions currently: i am also given the future temperature and element possibilities.
 
@@ -8,11 +8,12 @@
 var searchBtn = document.querySelector("#search")
 var cityInput = document.querySelector("#cityName");
 var currentWeatherContainer = document.querySelector('#currentWeather')
+var forecastContainer = document.querySelector('#forecast')
 
 function userInput() {
     var city = cityInput.value
     runCurrentWeather(city);
-    // runForecast(city);
+    runForecast(city);
 }
 
 function runCurrentWeather(city) {
@@ -23,23 +24,33 @@ function runCurrentWeather(city) {
     }).then(function (data) {
         currentWeatherContainer.innerHTML = '';
 
-        console.log(data);
-        console.log('cityName', data.name);
+        // console.log(data);
+        // console.log('cityName', data.name);
 
-        console.log('temp', data.main.temp);
-        console.log('humidity', data.main.humidity);
-        console.log('wind', data.wind.speed);
-        console.log('Weather Icon', data.weather[0].icon);
-        console.log('date', new Date(data.dt * 1000).toLocaleDateString());
+        // console.log('temp', data.main.temp);
+        // console.log('humidity', data.main.humidity);
+        // console.log('wind', data.wind.speed);
+        // console.log('Weather Icon', data.weather[0].icon);
+        // console.log('date', new Date(data.dt * 1000).toLocaleDateString());
 
         // create the elements that we will be displaying in the html
         var heading = document.createElement('h2')
+        var temp = document.createElement('p')
+        var humidity = document.createElement('p')
+        var wind = document.createElement('p')
+
+
 
         // add the text content to the elements
-        heading.textContent = data.name + ' ' + new Date(data.dt * 1000).toLocalDateString()
+        heading.textContent = data.name + ' ' + new Date(data.dt * 1000).toLocaleDateString()
+        temp.textContent = 'Temp: ' + data.main.temp + '°';
+        wind.textContent = 'Wind: ' + data.wind.speed + 'mph';
+        humidity.textContent = 'Humidity: ' + data.main.humidity + '%';
+
+
 
         // append the elements to the current weather container
-        currentWeatherContainer.append(heading)
+        currentWeatherContainer.append(heading, temp, humidity, wind)
     });
 }
 
@@ -49,12 +60,28 @@ function runForecast(city) {
     fetch(URL).then(function (response) {
         return response.json()
     }).then(function (data) {
-        runForecastContainer.innerHTML = ''; 
 
-        console.log(data);
-        console.log('cityName', data.name);
-        
-// Need to know how to change this for the forecast 
+        var forecastArray = [data.list[4], data.list[12], data.list[20], data.list[28], data.list[36]]
+        console.log(forecastArray);
+
+        for (let i = 0; i < forecastArray.length; i++) {
+            // create the elements that we will use for oue forecast boxes
+            var div = document.createElement('div')
+            var date = document.createElement('h3')
+            var temp = document.createElement('p')
+
+
+            // add the text content to the elements
+            temp.textContent = forecastArray[i].main.temp_max
+            date.textContent = new Date(forecastArray[i].dt_txt).toLocaleDateString()
+
+            // append the elements to the forecastContainer
+            div.append(temp, date)
+            forecastContainer.append(div)
+
+        }
+
+        // Need to know how to change this for the forecast 
         // console.log('temp', data.main.temp);
         // console.log('humidity', data.main.humidity);
         // console.log('wind', data.wind.speed);
